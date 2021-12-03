df3b88b4f1c9ba4b2e22a7b87e0035bea7e0eb52
Rui Guilherme Monteiro <a93179@alunos.uminho.pt>
2021-12-03 09:57:24 +0000

    m

--- ficha10_BB.pl
+++ ficha10_BB.pl
@@ -7,6 +7,7 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % jogo(IdJogo, Arbitro, AjudasCusto) -> {V,F,D}
 
+
 % i.
 jogo(IdJogo, Almeida_Antunes, 500);
 % ii.
@@ -15,3 +16,43 @@ jogo(IdJogo, Baltazar_Borges, xpto0123);
 excecao(jogo(Id,Arbitro,AjudasCusto)):-
 				jogo(IdJogo, Arbitro, xpto0123).
 
+
+
+% iii.
+
+
+excecao(jogo(IdJogo, CostaCarvalho, AjudasCusto)):-
+                    jogo(IdJogo, CostaCarvalho, 500),
+                    jogo(IdJogo, CostaCarvalho, 2000).
+
+
+
+
+% jogo(J3, CostaCarvalho, );
+
+% iv.
+
+% jogo(J4,DuarteDurao, );
+
+
+
+
+
+
+
+
+
+
+
+
+% Apontamentos da aula anterior (Segundo stor)
+si(Q,verdadeiro):-
+    Q.
+
+
+si(Q,falso):-
+    -Q.
+
+si(Q,desconhecido) :-
+     nao(Q),
+     nao(-Q).
