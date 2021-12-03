03dd963864cd1b35cd19387a0b2f0d4009330215
Rui Guilherme Monteiro <a93179@alunos.uminho.pt>
2021-12-03 09:35:40 +0000

    Ficha10

---
+++ ficha10_BB.pl
@@ -0,0 +1,17 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Ficha 10 -> Inteligência Artificial 2021/2022
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% jogo(IdJogo, Arbitro, AjudasCusto) -> {V,F,D}
+
+% i.
+jogo(IdJogo, Almeida_Antunes, 500);
+% ii.
+jogo(IdJogo, Baltazar_Borges, xpto0123);
+
+excecao(jogo(Id,Arbitro,AjudasCusto)):-
+				jogo(IdJogo, Arbitro, xpto0123).
+
